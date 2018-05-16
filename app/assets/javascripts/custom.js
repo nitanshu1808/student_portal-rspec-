f5c85dcdcda8282434124843bc05686cdd84ad6b
Nitanshu Rehani <nitanshurehani@nitanshus-air.home>
2018-05-16 23:28:52 +0100

    Error Handling

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -6,17 +6,21 @@ $(document).on('click', '#all-students', function(event ){
   var onDone = function (result) {
     var table = $(".table-dark")      
     var table_body = table.find('tbody')
-    var html       = "<tr>"
-    for (i = 0; i < result.length;i ++){
-      student = Object.values(result[i]);
-      for(j = 1; j < student.length - 2; j++){
-        html += "<td>" + student[j] + "</td>";
-      }
-      html += "</tr>"
-    }
     table.removeClass('hide');
     table_body.html("");
-    table_body.append(html);
+    if(result.length > 0){
+      var html       = "<tr>"
+      for (i = 0; i < result.length;i ++){
+        student = Object.values(result[i]);
+        for(j = 1; j < student.length - 2; j++){
+          html += "<td>" + student[j] + "</td>";
+        }
+        html += "</tr>"
+      }
+      table_body.append(html);
+    }else{
+      table.after('<p class = "text-center" style = "color: white">No Results Found</p>')
+    }
   }
 
   var onFail = function( err ) {
@@ -25,7 +29,6 @@ $(document).on('click', '#all-students', function(event ){
 
   ajaxCall('get', 'all_students', onDone, onFail, 'json');
 })
-
 
 function ajaxCall ( mType, url, doneCB, failCB, dataType) {
   var ajaxObj = {
