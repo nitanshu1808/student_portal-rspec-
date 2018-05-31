5e5bb631da20578ac72aca258bbc84e942dc0c9a
Nitanshu Rehani <nitanshurehani@Nitanshus-MacBook-Air.local>
2018-05-31 13:10:28 +0100

    - Moved hardcoded text to en.yml files

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2018_05_14_161806) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "students", force: :cascade do |t|
     t.string "name", null: false
     t.string "email", null: false
